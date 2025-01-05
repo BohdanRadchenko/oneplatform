abc1a6da96c6c490219e6235cef8d10c01f4dd8c
BohdanRadchenko <bradchenko@gmail.com>
2025-01-05 20:17:09 +0200

    added safe base button build

---
+++ packages/utils/build/cn/cn.mjs
@@ -0,0 +1,9 @@
+import { clsx as t } from "clsx";
+import { twMerge as o } from "tailwind-merge";
+function m(...r) {
+  return o(t(r));
+}
+export {
+  m as cn,
+  m as default
+};
---
+++ packages/utils/build/index.mjs
@@ -0,0 +1,4 @@
+import { cn as c } from "./cn/cn.mjs";
+export {
+  c as cn
+};
